38ee939b2f14c222c3609d2f9ea8c54ed8415e1f
Nicole Zheng <zznicole@hotmail.com>
2020-06-02 12:38:50 +0200

    articles fonts and layout

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
-// var url = 'http://newsapi.org/v2/everything?' +
-//           'q=Apple&' +
-//           'from=2020-06-01&' +
-//           'sortBy=popularity&' +
-//           'apiKey=14c0ac661fc44e2a9ee0f9c0c9af7ac4';
 
-// var req = new Request(url);
 
 const app =  new Vue ({
   el: '#vue-app',
@@ -39,24 +33,10 @@ const app =  new Vue ({
     fetch(req) 
     .then(response => response.json())
     .then(articlesResponse => {
-      this.articles = articlesResponse;
+      this.articles = articlesResponse.articles;
     })
-    
-    // var url = 'http://newsapi.org/v2/everything?' +
-    //       'q=Apple&' +
-    //       'from=2020-06-01&' +
-    //       'sortBy=popularity&' +
-    //       'apiKey=14c0ac661fc44e2a9ee0f9c0c9af7ac4';
 
-    // var req = new Request(url);
-    // fetch(req)
-    //   .then(response => response.json())
-    //   .then(json => console.log(json))
-//       .then(articlesResponse => {
-//         this.articles = articlesResponse.articles
-//         console.log(response);
-//       })
-fetch("")
+console.log(fetch(""));
       
   }
 })
@@ -64,28 +44,3 @@ fetch("")
 
  
 
-// var url = 'http://newsapi.org/v2/everything?' +
-//           'q=Apple&' +
-//           'from=2020-06-01&' +
-//           'sortBy=popularity&' +
-//           'apiKey=14c0ac661fc44e2a9ee0f9c0c9af7ac4';
-
-// var req = new Request(url);
-
-// fetch(req) 
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(response) {
-//       console.log(response);
-//     })
-
-// fetch("");
-
-
-// mounted: function() {
-//   fetch('the api url')
-//       .then(response => response.json())
-//       .then(articlesResponse => {
-//           this.articles = articlesResponse.articles
-//       })
